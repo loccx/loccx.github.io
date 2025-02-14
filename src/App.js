8298e83907f69994feebaf2859178800657938bf
loccx <kxingxu@gmail.com>
2025-02-13 19:15:12 -0800

    why the fuck isnt this working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [para, setPara] = useState('');
+  const paragraph = 'bitches';
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setPara((prev) => prev + paragraph[index]);
+      index++;
+      if (index === paragraph.length - 1) {
+        clearInterval(interval);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
+      <span data-typewriter = "on">Hello World!</span>
       <p>
-        hello bitches
+        {para}
       </p>
     </>
   );

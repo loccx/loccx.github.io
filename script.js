abeed9c25b4bbe693ef43b06927b1d553f9ab350
loccx <kxingxu@gmail.com>
2025-02-13 17:56:18 -0800

    button

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const text = "hello world";
-let index = 0;
+const text = "hello world"const { useState, useEffect } = React;
 
-function typeWriter() {
-    if (index < text.length) {
-        document.getElementById("letter").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeWriter, 150);
-    }
+function Typewriter() {
+  const [text, setText] = useState('');
+  const fullText = 'hello world';
+  let index = 0;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText((prev) => prev + fullText[index]);
+      index++;
+      if (index === fullText.length) clearInterval(interval);
+    }, 150);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div className="letter">{text}</div>;
 }
 
-function restartTyping() {
-    index = 0;
-    document.getElementById("letter").innerHTML = "";
-    typeWriter();
-}
-
-window.onload = typeWriter;
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Typewriter />);
+;

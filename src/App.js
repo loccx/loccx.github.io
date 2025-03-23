4274fc6e771dbaec5c735f31beaa0220236abae7
loccx <kxingxu@gmail.com>
2025-03-23 05:04:19 -0700

    gf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,52 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [para, setPara] = useState('');
-  const paragraph = 'hialice\nasdfasdfasdf';
+  const paragraph = `                                    o$$
+                                  $$$$$              ooo
+                                $$$$$$$$          o$$$\" \"\"oo
+                              o$$$\"$$\"$$ o\"$$$\"oo$\"$$$  $$o $
+                            o$$$$ $$$ o $o\"     o\"\"\"\"\"\"$o\"$$oo\"$
+                           o$$$o$o$$$\"  $o$$$$$\"\"\" oo$$$$$o$$$ \"
+                           $$$\"$$$$$$ ooo$$\"\"$\"  o$$$$$o \"$o\"$$
+                           $$$ o\"$$$$$\"  $$$\"ooo$ $$$\"\"$$o$$\" $o $
+                          \"$$$ooo$$$$   $oo$$\"\"$$$$$$  $o$\"$ $\"$ $
+                           \"$$\"$$$$$\"   $$$    o$$$$oo\"\"o$\"  $$ $ $
+                            $$\"o$\"\"$    $$$$    \"$$$$$o$$\"  o$\"$$\"$
+                             \"\"\" $$$    $$$$o    $$$o$o \"\"$$$  \"$
+                               oo$\"$$    $$$$$\" \"$$$$ $$ooooo\"\"\"\"\"
+                             o$$\"$\" $$o  $$$$$$   \"o$$o$
+                           o$\"\" $$oo$$$$o$$$$$$$$$o$$$$
+                         o\"$\" o$$\"  \"$$$$$$$\"$$\"$$$$$$$
+                        o$$o$\"\"\"$o    \"$$$o\"o\"o$$\"    \\"\\"
+                       $$$\"\"     \"\"o oooo\"\"\"\"\"\"
+                    $o$$\"          $$$$$$$$o
+                   o$$$            $\"$$$$o$\"\"$o
+                  o$$$\"           $$$$$$$$oo\"\" $
+                 o$oo$            $$\"$$$$$$$$\"$\"$
+                o$o\"$               \"$$$$$$$$$$ oo$
+                oo$$\"               \"\"$ $$$$$$$$$ $
+                $$\"$$                  \"ooo$\"\"$$$$$o
+                $$$\"                      \"\"$$o$\"$o\"
+                $$$                           \"\"\"\"$
+                $$$
+                $$
+                $$
+    oo         $$$
+   $$$         $$$  oooo$$ooo$o$oo
+  o$$$$o       $$o$$$$$$$$$$$$$$\"
+  $$$$$$o      $$$$$$$$$$$$$$$$\"
+ $$$$$$$$o     o$$$$$$$$$o$$$$\"
+ $$$$$$$$$o   $$$$$$$$$$$$$$\"
+$$$$$$$$$$$$ o$$$$$$$$$$$$\"
+$$$$$$$$$$$$ oo$\"$$$$$$\"\"\"
+ $$$$$$$$$$$$$\"$\n
+ \"$$$$$$$$$$$  $\n
+   \"\"$$$$\"$$$$ \"\n
+          $$ $$\"\n
+          $$ $$\n
+          $$ $$\n
+          \"$$$$\n
+           \"$$$\n`;
 
   useEffect(() => {
     let index = 0;
@@ -12,15 +57,15 @@ function App() {
       if (index === paragraph.length - 1) {
         clearInterval(interval);
       }
-    }, 100);
+    }, 10);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <>
-      <span data-typewriter = 'on'>Hello World!</span>
-      <p>
+      <span data-typewriter='on'>happy 1 month bae 🧍🏻 here is a rose</span>
+      <p style={{ whiteSpace: 'pre' }}>
         {para}
       </p>
     </>
